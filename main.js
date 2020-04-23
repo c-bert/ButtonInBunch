be0cce66e7f3ead6e0739866dec7aa6a921eb91e
c-bert <38592221+c-bert@users.noreply.github.com>
2020-04-23 15:44:06 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+// Add your javascript here
+'use strict';
+const buttonList = [b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b13, b14, b15, b16, b17, b18, b19, b20];
+function handleButtonClick(buttonList)
+{
+    let indexHolder = "";
+    //on button click
+        $(button).click(
+            //loop thru indeces
+            for (let i=1; i<=buttonList.length; i++){
+            alert("This button's index is: " + indexHolder)
+    }
+
+        );
+    return indexHolder;
+};
+
