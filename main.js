1a770e8d6042b703aee81bded1c769832f02bba0
c-bert <38592221+c-bert@users.noreply.github.com>
2020-04-23 16:45:32 -0400

    Restructured since I can't use jQuery

--- main.js
+++ main.js
@@ -1,14 +1,28 @@
+// Add your javascript here
 'use strict';
-const buttonList = ["#b1", b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b13, b14, b15, b16, b17, b18, b19, b20];
-function handleButtonClick(buttonList)
-{
-    let indexHolder = "";
-    //on button click
-        $(button).click(function(){
-            //loop thru indeces
-            for (let i=1; i<=buttonList.length; i++){
-            alert("This button's index is: " + indexHolder)
-            }
-        });
-    return indexHolder;
+//since you can't import jQuery
+//create global variables for each button
+const b1 = document.getElementById("b1");
+const b2 = document.getElementById("b2");
+const b3 = document.getElementById("b3");
+const b4 = document.getElementById("b4");
+
+const buttonList = [b1, b2, b3, b4];
+
+function indexMessage(buttonList){
+    let message = "";
+    if (document.onclick(function(){
+        message = "Button 1";
+        alert("Button 1");
+        console.log("Button 1");
+    }));
+    return message;
+    // `<section class="feedback" role="feedback">
+    //   <div id="message"> {message} </div>      
+    // </section>`;
+}
+document.getElementById('b1').onclick = function showMessageContent() {
+
+   console.log("Button 1")
+
 }
